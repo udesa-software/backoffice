40fe70527e4a4365bf7f6fa8f9b2dff0c3fd8d2c
TuteCostantini <tutecostantini@gmail.com>
2026-06-24 22:30:20 -0300

    saco threshold de tests para integracion

--- jest.integration.config.js
+++ jest.integration.config.js
@@ -24,12 +24,4 @@ module.exports = {
     'src/middlewares/authenticate.js',
     'src/middlewares/authorize.js',
   ],
-  coverageThreshold: {
-    global: {
-      branches: 85,
-      functions: 85,
-      lines: 85,
-      statements: 85,
-    },
-  },
 };
